3d5ed801264495ddc3497f9dfa44ba89f71cee96
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-11-20 23:54:19 +0600

    Создан компонент, который отображает информацию о человеке, включая имя, должность, название компании и еще три пункта

---
+++ 7_task/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonalInfo from './assets/PersonalInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalInfo />
+    </div>
+  );
+};
+
+export default App;
---
+++ 7_task/src/assets/PersonalInfo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';;
+
+const PersonalInfo = () => {
+    const person = {
+        name: "Темирбаева Бермет",
+        age: "25 лет",
+        position: "Junior Back-end developer",
+        company: "TechCompany",
+        experience: "3 года",
+        education: "Компьютерная инженерия",
+    };
+
+    return (
+        <div style={{ padding: '20px', border: '1px solid #ccc', borderRadius: '5px', maxWidth: '400px', margin: 'auto' }}>
+            <h1>Информация о человеке</h1>
+            <p>Имя: {person.name}</p>
+            <p>Возраст: {person.age}</p>
+            <p>Должность: {person.position}</p>
+            <p>Компания: {person.company}</p>
+            <p>Опыт работы: {person.experience}</p>
+            <p>Образование: {person.education}</p>
+        </div>
+    );
+};
+
+export default PersonalInfo;
