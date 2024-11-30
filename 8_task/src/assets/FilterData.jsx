654867fce6cbd248b23c3306f2fd2d3371fd3b60
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-12-01 00:41:17 +0600

    Инициализирован список данных при первом рендере с помощью useEffect

--- 8_task/src/assets/FilterData.jsx
+++ 8_task/src/assets/FilterData.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const FilterData = () => {
   const [data, setData] = useState([]); // Исходный список данных
   const [search, setSearch] = useState(""); // Строка поиска
 
+  useEffect(() => {
+    const fetchData = async () => {
+      // Создаём имтацию данных(рандомные 10 элементов)
+      const fakeData = [
+        "Sunrise",
+        "Dream",
+        "Mountain",
+        "River",
+        "Courage",
+        "Freedom",
+        "Star",
+        "Harmony",
+        "Whisper",
+        "Ocean",
+      ];
+      setData(fakeData);
+    };
+
+    fetchData();
+  }, []); 
+  
   return (
     <div>
       <h1>Data filter</h1>
